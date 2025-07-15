60dfd76706f6e0da2889df6f33ba1d0daccb3d56
Pedro Henrique Galvão dos Santos <pedro.santos@flieger.software>
2025-07-15 19:44:05 -0300

    Developments done in class 43 and 44 of Spryker Data Management  module
    Signed-off-by: Pedro Henrique Galvão dos Santos <pedro.santos@flieger.software>

---
+++ src/Pyz/Zed/AntelopeDataImport/Business/AntelopeDataImportBusinessFactory.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Pyz\Zed\AntelopeDataImport\Business;
+
+use Generated\Shared\Transfer\DataImporterConfigurationTransfer;
+use Pyz\Zed\AntelopeDataImport\Business\DataImportStep\AntelopeWriterStep;
+use Spryker\Zed\DataImport\Business\DataImportBusinessFactory;
+
+use Spryker\Zed\DataImport\Business\Model\DataImporterInterface;
+class AntelopeDataImportBusinessFactory extends DataImportBusinessFactory
+{
+    /**
+     * @param \Generated\Shared\Transfer\DataImporterConfigurationTransfer|null $dataImporterConfigurationTransfer
+     *
+     * @return \Spryker\Zed\DataImport\Business\Model\DataImporterInterface
+     */
+    public function createAntelopeDataImport(?DataImporterConfigurationTransfer $dataImporterConfigurationTransfer = null): DataImporterInterface
+    {
+        $dataImporter = $this->getCsvDataImporterFromConfig($dataImporterConfigurationTransfer);
+
+        $dataSetStepBroker = $this->createTransactionAwareDataSetStepBroker();
+        $dataSetStepBroker->addStep($this->createAntelopeWriterStep());
+
+        $dataImporter->addDataSetStepBroker($dataSetStepBroker);
+
+        return $dataImporter;
+    }
+
+    /**
+     * @return \Pyz\Zed\AntelopeDataImport\Business\DataImportStep\AntelopeWriterStep
+     */
+    public function createAntelopeWriterStep(): AntelopeWriterStep
+    {
+        return new AntelopeWriterStep();
+    }
+}
